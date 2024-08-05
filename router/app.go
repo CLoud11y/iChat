b5a1c7d4165b6e3a34d237f92ed8b0b1addd267e
CLoud11y <963165605@qq.com>
2024-08-05 18:07:39 +0800

    add swagger

--- router/app.go
+++ router/app.go
@@ -1,13 +1,25 @@
 package router
 
 import (
+	"iChat/docs"
 	"iChat/service"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
-	r.GET("/index", service.Index)
+	registerSwagger(r)
+	r.GET("/api/index", service.Index)
 	return r
+}
+
+func registerSwagger(r *gin.Engine) {
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Title = "api manager"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "localhost:8080"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
--- service/index.go
+++ service/index.go
@@ -6,6 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api
+// @Summary index
+// @Description index page
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} string “ok”
+// @Router /api/index [get]
 func Index(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "this is an index",
