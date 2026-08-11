2afc7ca488f9cda4adc80f1f0c5168ea76f641b2
CLoud11y <963165605@qq.com>
2026-08-11 19:20:26 +1000

    [feat] use docker compose to set mysql and redis

--- config/initconf.go
+++ config/initconf.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/spf13/viper"
 )
 
@@ -35,17 +39,56 @@ type Config struct {
 }
 
 func init() {
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("c:\\Users\\96316\\vscodeProjects\\goProjects\\iChat\\config")
+	configureConfigFile()
+	viper.SetEnvPrefix("ICHAT")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
-		panic("Error reading config file: " + err.Error())
+		panic("Error reading config file (copy config/config.example.yml to config/config.yml): " + err.Error())
 	}
 
 	if err := viper.Unmarshal(&Conf); err != nil {
 		panic("Unable to decode into struct: " + err.Error())
 	}
-	// fmt.Println(Conf.App)
-	// fmt.Println(Conf.MYSQL)
+}
+
+func configureConfigFile() {
+	if configFile := os.Getenv("ICHAT_CONFIG_FILE"); configFile != "" {
+		viper.SetConfigFile(configFile)
+		return
+	}
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	viper.AddConfigPath(".")
+
+	if workingDirectory, err := os.Getwd(); err == nil {
+		if projectRoot := findProjectRoot(workingDirectory); projectRoot != "" {
+			viper.AddConfigPath(filepath.Join(projectRoot, "config"))
+		}
+	}
+
+	if executable, err := os.Executable(); err == nil {
+		viper.AddConfigPath(filepath.Join(filepath.Dir(executable), "config"))
+	}
+}
+
+func findProjectRoot(start string) string {
+	directory, err := filepath.Abs(start)
+	if err != nil {
+		return ""
+	}
+
+	for {
+		if _, err := os.Stat(filepath.Join(directory, "go.mod")); err == nil {
+			return directory
+		}
+		parent := filepath.Dir(directory)
+		if parent == directory {
+			return ""
+		}
+		directory = parent
+	}
 }
